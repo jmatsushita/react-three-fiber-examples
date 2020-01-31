09a63e4f9324bae90af099e0a36fd4d1c3391d02
Jun Matsushita <jun@iilab.org>
2020-01-31 16:34:52 +0100

    Mock performance.now() to fix jmatsushita/react-three-fiber-examples#3

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { View, Button, YellowBox } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { useScreens } from 'react-native-screens';
-import { Stars } from './src/screens/Stars';
-import { Physics } from './src/screens/Physics';
-import { Reparenting } from './src/screens/Reparenting';
-import { MultiRender } from './src/screens/MultiRender';
-import { Montage } from './src/screens/Montage';
-import { Font } from './src/screens/Font';
-import { Lines } from './src/screens/Lines';
-import { MeshLine } from './src/screens/MeshLine';
-import { HelloWorld } from './src/screens/HelloWorld';
-import { ARKit } from './src/screens/ARKit';
+import React from "react";
+import { View, Button, YellowBox } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { useScreens } from "react-native-screens";
+import { Stars } from "./src/screens/Stars";
+import { Physics } from "./src/screens/Physics";
+import { Reparenting } from "./src/screens/Reparenting";
+import { MultiRender } from "./src/screens/MultiRender";
+import { Montage } from "./src/screens/Montage";
+import { Font } from "./src/screens/Font";
+import { Lines } from "./src/screens/Lines";
+import { MeshLine } from "./src/screens/MeshLine";
+import { HelloWorld } from "./src/screens/HelloWorld";
+import { ARKit } from "./src/screens/ARKit";
 
 useScreens();
 
 YellowBox.ignoreWarnings([
-  'Accessing view manager configs',
-  'Warning: Can\'t perform a React state update'
+  "Accessing view manager configs",
+  "Warning: Can't perform a React state update"
 ]);
 
 window.performance = {
@@ -26,49 +26,57 @@ window.performance = {
   measure() {},
   clearMeasures() {},
   mark() {},
-}
+  now() {}
+};
 
 const menuItems = [
-  { key: 'HelloWorld', value: 'Hello World' },
-  { key: 'Physics', value: 'Physics' },
-  { key: 'Stars', value: 'Stars' },
-  { key: 'Reparenting', value: 'Reparenting' },
-  { key: 'MultiRender', value: 'Multi Render'},
-  { key: 'Montage', value: 'Montage' },
-  { key: 'Font', value: 'Font' },
-  { key: 'Lines', value: 'Lines' },
-  { key: 'MeshLine', value: 'MeshLine' },
-  { key: 'ARKit', value: 'ARKit' }
+  { key: "HelloWorld", value: "Hello World" },
+  { key: "Physics", value: "Physics" },
+  { key: "Stars", value: "Stars" },
+  { key: "Reparenting", value: "Reparenting" },
+  { key: "MultiRender", value: "Multi Render" },
+  { key: "Montage", value: "Montage" },
+  { key: "Font", value: "Font" },
+  { key: "Lines", value: "Lines" },
+  { key: "MeshLine", value: "MeshLine" },
+  { key: "ARKit", value: "ARKit" }
 ];
 
 function Main(props) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       {menuItems.map(({ key, value }) => (
-        <Button key={key} title={value} onPress={() => props.navigation.push(key)} />
+        <Button
+          key={key}
+          title={value}
+          onPress={() => props.navigation.push(key)}
+        />
       ))}
     </View>
   );
 }
 
 Main.navigationOptions = {
-  title: 'react-three-fiber examples',
+  title: "react-three-fiber examples"
 };
 
-const AppNavigator = createStackNavigator({
-  Main,
-  HelloWorld,
-  Physics,
-  Stars,
-  Reparenting,
-  MultiRender,
-  Montage,
-  Font,
-  Lines,
-  MeshLine,
-  ARKit
-}, {
-  initialRouteName: 'Main',
-});
+const AppNavigator = createStackNavigator(
+  {
+    Main,
+    HelloWorld,
+    Physics,
+    Stars,
+    Reparenting,
+    MultiRender,
+    Montage,
+    Font,
+    Lines,
+    MeshLine,
+    ARKit
+  },
+  {
+    initialRouteName: "Main"
+  }
+);
 
 export default createAppContainer(AppNavigator);
